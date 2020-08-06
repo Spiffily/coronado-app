56ddd0fdb706512a3c947f8be7a2f9937050eda7
Josh <sillynoodle36@gmail.com>
2020-08-05 18:51:12 -0600

    Worked on themes, added icons, and added coronado theme.

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: "./assets/icon.png",
+    // webSecurity: false,
+    // allowRunningInsecureContent: true,
+    // experimentalFeatures: true,
+    // offscreen: true,
   })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -41,4 +46,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
